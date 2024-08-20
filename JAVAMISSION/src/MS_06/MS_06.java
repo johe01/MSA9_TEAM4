fe1f9ffbd46fe193d74a5469da1f8931aa32ac58
johe01 <jhe0598@gmail.com>
2024-08-20 14:30:32 +0900

    로또리아N게임

--- JAVAMISSION/src/MS_06/MS_06.java
+++ JAVAMISSION/src/MS_06/MS_06.java
@@ -1,46 +1,49 @@
 package MS_06;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class MS_06 {
 
 	public static void main(String[] args) {
-		int arr[] = new int[6]; // 배열 생성
-
+		
 		System.out.print("몇 게임? ");
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt(); // 몇 게임할지 입력받음
+		int n=sc.nextInt();
 		sc.close();
-		for (int sum = 0; sum < n; sum++) {
-			// 입력 받은 수만큼 반복
-			arr[0] = (int) (Math.random() * 45) + 1;
-			System.out.println("arr[0]: " + arr[0]);
-			System.out.printf("[%d 게임] : ", sum + 1);
-			for (int i = 1; i < arr.length; i++) {
-				int rand = (int) (Math.random() * 45) + 1;
-				System.out.println("r( " + i + " ) : " + rand);
-				for (int j = i - 1; j >= 0; j--) {
-					if (arr[j] == rand) {
+		
+		int lotto[] = new int[6];
+		
+		for (int m = 0; m < n; m++) {
+			System.out.printf("[%d 게임]: ",m+1);
+			
+			for (int i = 0; i < 6; i++) {
+				int random = (int)(Math.random() * 45 + 1);
+				lotto[i]= random;
+				// 중복 제거
+				for (int j=0; j<i; j++) {
+					// 중복이 되는 경우
+					if(lotto[j]== random) {
 						i--;
-						break;
-					}
-					else if (rand < arr[j]) {
-						arr[j + 1] = arr[j];
-						arr[j] = rand;
-					} else if (rand > arr[j]) {
-						arr[j + 1] = rand;
-						break;
 					}
 				}
-//				arr[i]= rand;
 			}
-
-			for (int i = 0; i < arr.length; i++) {
-				System.out.print(arr[i] + " ");
+			
+			// 오름차순 정렬
+			for (int i = 0; i < lotto.length - 1; i++) {
+				for (int j = i + 1; j < lotto.length; j++) {
+					if (lotto[i] > lotto[j]) {
+						// swap
+						// lotto[i]와 lotto[j] 교환
+						int temp = lotto[i];
+						lotto[i] = lotto[j];
+						lotto[j] = temp;
+					}
+				}
+			}
+			for (int i = 0; i < lotto.length; i++) {
+				System.out.print(lotto[i] + " ");
 			}
 			System.out.println();
-
 		}
 
 	}
