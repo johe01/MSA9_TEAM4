17257a1b6bb756baff98f4b9febdb7223a287665
johe01 <jhe0598@gmail.com>
2024-08-21 18:17:56 +0900

    13번 미완성

---
+++ JAVAMISSION/src/MS_13/MS_13.java
@@ -0,0 +1,58 @@
+package MS_13;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class MS_13 {
+
+public static void main(String[] args) {
+		
+		System.out.print("몇 게임? ");
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		ArrayList<ArrayList<Integer>> lotto = new ArrayList<ArrayList<Integer>>(); 
+		
+		for (int m = 0; m < n; m++) {
+			
+			System.out.printf("[%d 게임] (1.자동 / 2.수동) : ",m+1);
+			int choice = sc.nextInt();
+			
+			if (choice==1) {
+				lotto.add(m, new ArrayList<Integer>()); 
+				for (int i = 0; i < 6; i++) {
+					int random = (int)(Math.random() * 45 + 1);
+					lotto.get(m).add(random);
+					
+					for (int j = 0; j < i; j++) {
+						if (lotto.get(m).get(j)==random) {
+							lotto.get(m).remove(i);
+							i--;
+						}
+					}
+				}
+				// 정렬 메소드 사용
+				lotto.get(m).sort(Comparator.naturalOrder());				
+			}
+			else if (choice==2) {
+				lotto.add(m, new ArrayList<Integer>()); 
+				for (int i = 0; i < 6; i++) {
+					System.out.printf("%d : ",i+1);
+					int num = sc.nextInt();
+					lotto.get(m).add(num);
+				}
+			}
+			else {
+				System.out.println("1 과 2 중에 선택해주세요.");
+				m--;
+				continue;
+			}
+			for (int i = 0; i < lotto.get(m).size(); i++) {
+				System.out.print(lotto.get(m).get(i)+" ");
+			}
+			System.out.println();
+			
+		}
+		sc.close();
+	}
+}
