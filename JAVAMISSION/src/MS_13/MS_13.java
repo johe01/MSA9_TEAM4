0ea35eba328bb12c5522bde5681272b6d168f4ad
johe01 <jhe0598@gmail.com>
2024-08-26 18:27:27 +0900

    13번 미완성

--- JAVAMISSION/src/MS_13/MS_13.java
+++ JAVAMISSION/src/MS_13/MS_13.java
@@ -8,51 +8,54 @@ public class MS_13 {
 
 public static void main(String[] args) {
 		
-		System.out.print("몇 게임? ");
-		Scanner sc = new Scanner(System.in);
-		int n=sc.nextInt();
-		ArrayList<ArrayList<Integer>> lotto = new ArrayList<ArrayList<Integer>>(); 
+//		System.out.print("몇 게임? ");
+//		Scanner sc = new Scanner(System.in);
+//		int n=sc.nextInt();
+//		ArrayList<ArrayList<Integer>> lotto = new ArrayList<ArrayList<Integer>>(); 
+//		
+//		for (int m = 0; m < n; m++) {
+//			
+//			System.out.printf("[%d 게임] (1.자동 / 2.수동) : ",m+1);
+//			int choice = sc.nextInt();
+//			
+//			if (choice==1) {
+//				lotto.add(m, new ArrayList<Integer>()); 
+//				for (int i = 0; i < 6; i++) {
+//					int random = (int)(Math.random() * 45 + 1);
+//					lotto.get(m).add(random);
+//					
+//					for (int j = 0; j < i; j++) {
+//						if (lotto.get(m).get(j)==random) {
+//							lotto.get(m).remove(i);
+//							i--;
+//						}
+//					}
+//				}
+//				// 정렬 메소드 사용
+//				lotto.get(m).sort(Comparator.naturalOrder());				
+//			}
+//			else if (choice==2) {
+//				lotto.add(m, new ArrayList<Integer>()); 
+//				for (int i = 0; i < 6; i++) {
+//					System.out.printf("%d : ",i+1);
+//					int num = sc.nextInt();
+//					lotto.get(m).add(num);
+//				}
+//			}
+//			else {
+//				System.out.println("1 과 2 중에 선택해주세요.");
+//				m--;
+//				continue;
+//			}
+//			for (int i = 0; i < lotto.get(m).size(); i++) {
+//				System.out.print(lotto.get(m).get(i)+" ");
+//			}
+//			System.out.println();
+//			
+//		}
+//		sc.close();
 		
-		for (int m = 0; m < n; m++) {
-			
-			System.out.printf("[%d 게임] (1.자동 / 2.수동) : ",m+1);
-			int choice = sc.nextInt();
-			
-			if (choice==1) {
-				lotto.add(m, new ArrayList<Integer>()); 
-				for (int i = 0; i < 6; i++) {
-					int random = (int)(Math.random() * 45 + 1);
-					lotto.get(m).add(random);
-					
-					for (int j = 0; j < i; j++) {
-						if (lotto.get(m).get(j)==random) {
-							lotto.get(m).remove(i);
-							i--;
-						}
-					}
-				}
-				// 정렬 메소드 사용
-				lotto.get(m).sort(Comparator.naturalOrder());				
-			}
-			else if (choice==2) {
-				lotto.add(m, new ArrayList<Integer>()); 
-				for (int i = 0; i < 6; i++) {
-					System.out.printf("%d : ",i+1);
-					int num = sc.nextInt();
-					lotto.get(m).add(num);
-				}
-			}
-			else {
-				System.out.println("1 과 2 중에 선택해주세요.");
-				m--;
-				continue;
-			}
-			for (int i = 0; i < lotto.get(m).size(); i++) {
-				System.out.print(lotto.get(m).get(i)+" ");
-			}
-			System.out.println();
-			
-		}
-		sc.close();
+		System.out.println("############# 인생역전 Lottoria ############");
+		System.out.println("발행일\t: ");
 	}
 }
