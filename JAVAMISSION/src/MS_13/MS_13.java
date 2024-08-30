7b6dfc2401d9e64c00cc2712f40606813dfc3de5
johe01 <jhe0598@gmail.com>
2024-08-30 15:45:52 +0900

    로또리아 당첨확인

--- JAVAMISSION/src/MS_13/MS_13.java
+++ JAVAMISSION/src/MS_13/MS_13.java
@@ -12,6 +12,8 @@ public class MS_13 {
 
 	private static MS_13 instance = new MS_13();
 	
+	// private static ArrayList<Integer> lottoList = new ArrayList<Integer>();
+	
 	private MS_13() {}
 	
 	public static MS_13 getInstance() {
@@ -111,7 +113,92 @@ public class MS_13 {
 		
 	}
 
+	public static void lottoDraw(ArrayList<Integer> lottoList) {
+		System.out.print("당첨 번호 : ");
+		// 랜덤 수 6개를 대입
+		for (int j = 0; j < 7; j++) {
+			int rand = (int) (Math.random() * 45 + 1); // 1~45 랜덤 수
 
+			// 중복 제거
+			if (lottoList.contains(rand))
+				j--;
+			else
+				lottoList.add(rand);
+
+		} // -- 랜덤 수 6개 대입 끝
+
+		// 정렬
+		Collections.sort(lottoList);
+
+		// 출력
+		for (int i = 0; i < 6; i++) {
+			System.out.print(lottoList.get(i) + " ");
+		}
+		System.out.println();
+		System.out.println("보너스 번호 : " + lottoList.get(6));
+		
+	}
+
+	public void resPrint(ArrayList<ArrayList<Integer>> lotto, ArrayList<String> randOption,
+			ArrayList<Integer> lottoList) {
+		char order = 'A';
+		System.out.println("#################### 당첨 결과 ####################");
+		for (int i = 0; i < lotto.size(); i++) {
+			System.out.print(order++);
+			System.out.print(" "+randOption.get(i)+" ");
+			for (int j = 0; j < 6; j++) {
+				System.out.printf("%02d    ",lotto.get(i).get(j));
+			}
+			lottoRes(lotto.get(i),lottoList);
+			
+		}
+		System.out.println("################################################");
+	}
+
+	public void printReceipt(ArrayList<ArrayList<Integer>> lotto, ArrayList<String> randOption) {
+		Calendar cal = Calendar.getInstance();
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd일 (E) HH:mm:ss");
+		String today = sdf.format(cal.getTime());
+		
+		if (cal.get(cal.DAY_OF_WEEK)==7&&cal.get(cal.HOUR_OF_DAY)>=21) {
+			cal.set(cal.DAY_OF_YEAR, (cal.get(cal.DAY_OF_YEAR)+5));
+			cal.get(cal.DAY_OF_YEAR);
+		}
+		
+		cal.set(cal.DAY_OF_WEEK, 7);
+		cal.set(cal.HOUR_OF_DAY, 21);
+		cal.set(cal.MINUTE, 0);
+		cal.set(cal.SECOND, 0);
+		String Dday = sdf.format(cal.getTime());
+		
+		int year = cal.get(cal.YEAR);
+		int day = cal.get(cal.DAY_OF_MONTH);
+		cal.set(cal.YEAR, year + 1);
+		cal.set(cal.DAY_OF_MONTH, day + 1);
+		SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy/MM/dd일 (E)");
+		String dLine = sdf2.format(cal.getTime());
+
+		System.out.println("############# 인생역전 Lottoria ############");
+		System.out.println("발행일\t\t: " + today);
+		System.out.println("추첨일\t\t: " + Dday);
+		System.out.println("지급기한\t\t: " + dLine);
+		System.out.println("--------------------------------------------------");
+		char alph = 'A';
+		for (int i = 0; i < lotto.size(); i++) {
+			System.out.print(alph + " " + randOption.get(i) + " ");
+			for (int j = 0; j < 6; j++) {
+				System.out.printf("%02d\t", lotto.get(i).get(j));
+			}
+			System.out.println();
+			alph++;
+		}
+		System.out.println("--------------------------------------------------");
+		DecimalFormat df = new DecimalFormat("\u00A4#,###");
+		System.out.println("금액\t\t\t\t\t  " + df.format(lotto.size() * 1000));
+		System.out.println("##################################################");
+		System.out.println();
+	}
 	
 	
 }
--- JAVAMISSION/src/MS_13/MS_13_Test.java
+++ JAVAMISSION/src/MS_13/MS_13_Test.java
@@ -1,95 +1,34 @@
 package MS_13;
 
-import java.text.DecimalFormat;
-import java.text.SimpleDateFormat;
+//import java.text.DecimalFormat;
+//import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Scanner;
+//import java.util.Calendar;
+//import java.util.Collections;
+//import java.util.Comparator;
+//import java.util.Scanner;
 
 public class MS_13_Test {
 
 	public static void main(String[] args) {
+		// 응모한 로또 번호 저장하는 ArrayList
+		ArrayList<ArrayList<Integer>> lotto = new ArrayList<ArrayList<Integer>>();
+		
+		// 자동/수동 옵션 여부 저장 하는 ArrayList
+		ArrayList<String> randOption = new ArrayList<String>();			
+		
+		// 당첨 번호 저장하는 ArrayList
+		ArrayList<Integer> lottoList = new ArrayList<Integer>();		
+		
 		
 		MS_13 lottoria = MS_13.getInstance();
-		ArrayList<ArrayList<Integer>> lotto = new ArrayList<ArrayList<Integer>>();
-		ArrayList<String> randOption = new ArrayList<String>();
-		MS_13.buyLotto(lotto, randOption); // - 입력 완료
 
-		// 당첨번호
-		Calendar cal = Calendar.getInstance();
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd일 (E) HH:mm:ss");
-		String today = sdf.format(cal.getTime());
-		SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy/MM/dd일 (E)");
-		String dLine = sdf2.format(cal.getTime());
+		lottoria.buyLotto(lotto, randOption); 	// 복권번호 지정 메소드
 
-		System.out.println("############# 인생역전 Lottoria ############");
-		System.out.println("발행일\t\t: " + today);
-		cal.set(cal.DAY_OF_WEEK, 7);
-		cal.set(cal.HOUR_OF_DAY, 21);
-		cal.set(cal.MINUTE, 0);
-		cal.set(cal.SECOND, 0);
-		String Dday = sdf.format(cal.getTime());
-		System.out.println("추첨일\t\t: " + Dday);
-		int year = cal.get(cal.YEAR);
-		int day = cal.get(cal.DAY_OF_MONTH);
-		cal.set(cal.YEAR, year + 1);
-		cal.set(cal.DAY_OF_MONTH, day + 1);
-		System.out.println("지급기한\t\t: " + dLine);
-		System.out.println("--------------------------------------------------");
-		char alph = 'A';
-		for (int i = 0; i < lotto.size(); i++) {
-			System.out.print(alph + " " + randOption.get(i) + " ");
-			for (int j = 0; j < 6; j++) {
-				System.out.printf("%02d\t", lotto.get(i).get(j));
-			}
-			System.out.println();
-			alph += 1;
-		}
-		System.out.println("--------------------------------------------------");
-		DecimalFormat df = new DecimalFormat("\u00A4#,###");
-		System.out.println("금액\t\t\t\t\t  " + df.format(lotto.size() * 1000));
-		System.out.println("##################################################");
-		System.out.println();
+		lottoria.printReceipt(lotto, randOption); 	// 지정한 복권 번호, 날짜 출력 메소드
 		
-
-		ArrayList<Integer> lottoList = new ArrayList<Integer>();
-
-		System.out.print("당첨 번호 : ");
-		// 랜덤 수 6개를 대입
-		for (int j = 0; j < 7; j++) {
-			int rand = (int) (Math.random() * 45 + 1); // 1~45 랜덤 수
-
-			// 중복 제거
-			if (lottoList.contains(rand))
-				j--;
-			else
-				lottoList.add(rand);
-
-		} // -- 랜덤 수 6개 대입 끝
-
-		// 정렬
-		Collections.sort(lottoList);
-
-		// 출력
-		for (int i = 0; i < 6; i++) {
-			System.out.print(lottoList.get(i) + " ");
-		}
-		System.out.println();
-		System.out.println("보너스 번호 : " + lottoList.get(6));
+		lottoria.lottoDraw(lottoList); 	// 번호 추첨 후 출력 메소드
 		
-		char order = 'A';
-		System.out.println("#################### 당첨 결과 ####################");
-		for (int i = 0; i < lotto.size(); i++) {
-			System.out.print(order++);
-			System.out.print(" "+randOption.get(i)+" ");
-			for (int j = 0; j < 6; j++) {
-				System.out.printf("%02d\t",lotto.get(i).get(j));
-			}
-			lottoria.lottoRes(lotto.get(i),lottoList);
-			
-		}
-		System.out.println("################################################");
+		lottoria.resPrint(lotto, randOption, lottoList);	// 당첨 결과 출력 메소드
 	}
 }
